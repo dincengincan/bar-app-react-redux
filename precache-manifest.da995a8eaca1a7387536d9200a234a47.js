03a1e7f51b91326c026397b3ba3e27b48aac24da
dincengincan <engincandinc@gmail.com>
2020-01-06 02:30:56 +0300

    Updates

---
+++ precache-manifest.da995a8eaca1a7387536d9200a234a47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d6b16ed52588d5d84912790997af72",
+    "url": "/bar-app-react-redux/index.html"
+  },
+  {
+    "revision": "9eeabdbb1667426df4ee",
+    "url": "/bar-app-react-redux/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d99de919c1151a4f6daf",
+    "url": "/bar-app-react-redux/static/css/main.4a8de1a6.chunk.css"
+  },
+  {
+    "revision": "9eeabdbb1667426df4ee",
+    "url": "/bar-app-react-redux/static/js/2.1900a85e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/bar-app-react-redux/static/js/2.1900a85e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d99de919c1151a4f6daf",
+    "url": "/bar-app-react-redux/static/js/main.904b1a1e.chunk.js"
+  },
+  {
+    "revision": "a68aee4dc9e00ec780cd",
+    "url": "/bar-app-react-redux/static/js/runtime-main.003ffc3d.js"
+  }
+]);
